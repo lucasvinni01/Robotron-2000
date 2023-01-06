3bfb04a6541bf4f0f3d4b606ec05d11fd5391666
lucasvinni01 <116289902+lucasvinni01@users.noreply.github.com>
2023-01-06 17:49:25 -0300

    Alterei o local das pastas

---
+++ main.js
@@ -0,0 +1,61 @@
+const estatisticas = document.querySelectorAll("[data-estatistica]")
+const controle = document.querySelectorAll("[data-controle]")
+const pecas = {
+    "bracos": {
+        "forca": 29,
+        "poder": 35,
+        "energia": -21,
+        "velocidade": -5
+    },
+    "blindagem": {
+        "forca": 41,
+        "poder": 20,
+        "energia": 0,
+        "velocidade": -20
+    },
+    "nucleos": {
+        "forca": 0,
+        "poder": 7,
+        "energia": 48,
+        "velocidade": -4
+    },
+    "pernas": {
+        "forca": 27,
+        "poder": 21,
+        "energia": -32,
+        "velocidade": 43
+    },
+    "foguetes": {
+        "forca": 0,
+        "poder": 28,
+        "energia": 0,
+        "velocidade": -2
+    }
+}
+
+controle.forEach ((elemento) => {
+    elemento.addEventListener("click", (evento) => {
+        manipulaDados(evento.target.dataset.controle, evento.target.parentNode)
+        atualizaEstatistica(evento.target.dataset.peca)
+    })
+})
+
+function manipulaDados(operacao, controle) {
+    const peca = controle.querySelector("[data-contador]")
+    
+
+    if (operacao === "-") {
+        peca.value = parseInt(peca.value) - 1
+    }
+    else {
+        peca.value = parseInt(peca.value) + 1
+    }
+}
+
+function atualizaEstatistica(peca) {
+    estatisticas.forEach( (elemento) => {
+        console.log(pecas[peca] [elemento.dataset.estatistica])
+        elemento.textContent = parseInt(elemento.textContent) + pecas[peca] [elemento.dataset.estatistica]
+    }
+
+)}
